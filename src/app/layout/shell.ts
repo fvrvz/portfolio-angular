0c8549b5268d3a0c705fee62cb38cbaa57648022
fvrvz <codefaraaz@gmail.com>
2025-06-09 00:17:53 +0530

    feat: add container

--- src/app/layout/shell.ts
+++ src/app/layout/shell.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navbar } from '../components/navbar/navbar';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from '../components/footer/footer';
+import { Container } from '../components/container/container';
 
 @Component({
   selector: 'app-shell',
-  imports: [Navbar, RouterOutlet, Footer],
+  imports: [Navbar, RouterOutlet, Footer, Container],
   templateUrl: './shell.html',
   styleUrl: './shell.scss',
 })
